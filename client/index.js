8b275857d72a21f27c1e209611c2c20be0283298
lizuncong <2900170896@qq.com>
2023-05-13 21:16:20 +0800

    feat: 连接

--- client/index.js
+++ client/index.js
@@ -1,7 +1,75 @@
+const SIGNAL_TYPE_JOIN = "join";
+const SIGNAL_TYPE_RESP_JOIN = "resp-join";
+const SIGNAL_TYPE_LEAVE = "leave";
+const SIGNAL_TYPE_NEW_PEER = "new-peer";
+const SIGNAL_TYPE_PEER_LEAVE = "peer-leave";
+const SIGNAL_TYPE_OFFER = "offer";
+const SIGNAL_TYPE_ANSWER = "answer";
+const SIGNAL_TYPE_CANDIDATE = "candidate";
+
 let localStream = null;
+const localUserId = Math.random().toString(36).substring(2);
+let remoteUserId;
+
+class BroadCaster {
+  constructor(wsUrl) {
+    this.wsUrl = wsUrl;
+    this.websocket = null;
+    this.createWebsocket();
+  }
+  createWebsocket() {
+    this.websocket = new WebSocket(this.wsUrl);
+    this.websocket.onopen = (e) => {
+      console.log("ws open", e);
+    };
+    this.websocket.onmessage = (e) => {
+      console.log("ws onmessage", e.data);
+      const jsonMsg = JSON.parse(e.data);
+      switch (jsonMsg.cmd) {
+        case SIGNAL_TYPE_NEW_PEER:
+          this.handleRemoteNewPeer(jsonMsg);
+          break;
+        case SIGNAL_TYPE_RESP_JOIN:
+          this.handleRemoteRespJoin(jsonMsg);
+          break;
+      }
+    };
+    this.websocket.onerror = (e) => {
+      console.log("ws onerror", e);
+    };
+    this.websocket.onclose = (e) => {
+      console.log("ws onclose", e);
+    };
+  }
+  sendMessage(msg) {
+    this.websocket.send(JSON.stringify(msg));
+  }
+  handleRemoteNewPeer(msg) {
+    console.log("有新的客户端连接进来了...", msg);
+    // createOffer();
+  }
+  handleRemoteRespJoin(msg) {
+    console.log('加入房间成功，房间内其他成员信息：', msg)
+  }
+}
+
+const broadCaster = new BroadCaster("ws://localhost:8088");
+
+const doJoin = (roomId) => {
+  const jsonMsg = {
+    cmd: "join",
+    roomId: roomId,
+    uid: localUserId,
+  };
+  broadCaster.sendMessage(jsonMsg);
+};
 
 joinBtn.onclick = () => {
   console.log("点击加入按钮");
+  if (!roomId.value) {
+    alert("请输入房间ID");
+    return;
+  }
   // 初始化本地码流
   navigator.mediaDevices
     .getUserMedia({
@@ -10,6 +78,7 @@ joinBtn.onclick = () => {
     })
     .then((stream) => {
       console.log("stream...", stream);
+      doJoin(roomId.value);
       localVideo.srcObject = stream;
       localStream = stream;
     })
---
+++ server/index.js
@@ -0,0 +1,73 @@
+const ws = require("nodejs-websocket");
+
+const SIGNAL_TYPE_JOIN = "join";
+const SIGNAL_TYPE_RESP_JOIN = "resp-join";
+const SIGNAL_TYPE_LEAVE = "leave";
+const SIGNAL_TYPE_NEW_PEER = "new-peer";
+const SIGNAL_TYPE_PEER_LEAVE = "peer-leave";
+const SIGNAL_TYPE_OFFER = "offer";
+const SIGNAL_TYPE_ANSWER = "answer";
+const SIGNAL_TYPE_CANDIDATE = "candidate";
+const port = 8088;
+
+const roomMap = {}; // { roomId: { uid: conn }}
+const handleJoin = (message, conn) => {
+  const { roomId, uid } = message;
+  console.log("加入房间...", roomId, uid);
+  let room = roomMap[roomId];
+  if (!room) {
+    room = roomMap[roomId] = {};
+  }
+  if (Object.keys(room).length >= 2) {
+    console.error("roomId:", roomId, "已经存在两个人，请使用其他房间");
+    return;
+  }
+  room[uid] = conn;
+  const uids = Object.keys(room);
+  // 对于房间中的所有用户
+  // 1.将当前新连接的用户信息广播给其他客户端
+  // 2.将当前房间内的所有用户信息广播给新连接的客户端
+  if (uids.length > 1) {
+    uids.forEach((remoteUid) => {
+      if (remoteUid === uid) return;
+      // 将当前用户的信息广播到其他客户端
+      let jsonMsg = {
+        cmd: SIGNAL_TYPE_NEW_PEER,
+        remoteUid: uid,
+      };
+      console.log("将当前新连接的用户信息广播给其他客户端..", jsonMsg);
+      room[remoteUid].sendText(JSON.stringify(jsonMsg));
+
+      jsonMsg = {
+        cmd: SIGNAL_TYPE_RESP_JOIN,
+        remoteUid,
+      };
+      console.log("将当前房间内的所有用户信息广播给新连接的客户端..", jsonMsg);
+      conn.sendText(JSON.stringify(jsonMsg));
+    });
+  }
+};
+
+const server = ws
+  .createServer((conn) => {
+    console.log("创建一个新的连接========");
+    conn.on("text", (str) => {
+      console.log("recv msg:", str);
+      const jsonMsg = JSON.parse(str);
+
+      switch (jsonMsg.cmd) {
+        case SIGNAL_TYPE_JOIN:
+          handleJoin(jsonMsg, conn);
+          break;
+        default:
+          console.error("无法识别的指令");
+      }
+    });
+    conn.on("close", (code, reason) => {
+      console.log("连接关闭", code, "reason:", reason);
+    });
+    conn.on("error", (err) => {
+      console.log("监听到错误", err);
+    });
+  })
+  .listen(port);
